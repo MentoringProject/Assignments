a8b7b6dd8886016a1cc9ec5679115b5b6a6f41bf
SchruteFarmsCode <szymon.teperek99@o2.pl>
2023-12-30 16:10:48 +0100

    Corrected class main, moved class MyStack to new package collections and removed file Assignments.iml from remote repository

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,6 @@
-import fundamentals.MyStack;
-
 public class Main {
 
     public static void main(String[] args) {
-        MyStack stackImplementation = new MyStack();
-
-        stackImplementation.push(2);
-        stackImplementation.push(12);
-        stackImplementation.push(22);
-        stackImplementation.display();
-
-        System.out.println("Pop: " + stackImplementation.pop());
-        System.out.println("IsEmpty: " + stackImplementation.isEmpty());
-        System.out.println("Size: " + stackImplementation.size());
-        System.out.println("Peek: " + stackImplementation.peek());
-        stackImplementation.display();
+        System.out.println("Hello");
     }
 }
--- src/main/java/fundamentals/collections/MyStack.java
+++ src/main/java/fundamentals/collections/MyStack.java
@@ -1,4 +1,4 @@
-package fundamentals;
+package fundamentals.collections;
 
 import java.util.ArrayList;
 import java.util.EmptyStackException;
